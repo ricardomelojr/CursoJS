7a2b14c16a3339fe8c843080a81186ba64c74b06
Ricardo Melo <ricardomelojunior@gmail.com>
2023-03-21 20:42:07 -0400

    arrow-function

--- section-two/31-funcoes-basico/index.js
+++ section-two/31-funcoes-basico/index.js
@@ -1,5 +1,5 @@
 function saudacao(nome) {
-   return `Bom dia ${nome}!`;
+    return `Bom dia ${nome}!`;
 }
 
 const variavel = saudacao('Ricardo');
@@ -12,3 +12,18 @@ function soma(x, y) {
 const resultado = soma(2, 5);
 
 console.log(resultado);
+
+
+// variável recebendo função
+const raiz = function (n) {
+    return Math.sqrt(n);
+};
+
+console.log(raiz(9));
+
+// arrow function
+const raiz_q = (n) => {
+    return Math.sqrt(n);
+};
+
+console.log(raiz_q(9));
