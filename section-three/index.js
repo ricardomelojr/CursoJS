e31d86b5f15d591b5d0c768a6c2415fcad688886
Ricardo Melo <ricardomelojunior@gmail.com>
2023-04-12 13:00:37 -0400

    Operadores de comparação

---
+++ section-three/index.js
@@ -0,0 +1,14 @@
+/**
+ * Operadores de compração
+ * > maior que
+ * >= maior que ou igual a
+ * < menor que
+ * <= menor que ou igual a
+ * == igualdade (valor)
+ * === igualdade estrita (valor e tipo)
+ * != diferente (valor)
+ * !== diferente estrito (valor e tipo) 
+ */
+
+const comp = 10 > 5;
+console.log(comp);
